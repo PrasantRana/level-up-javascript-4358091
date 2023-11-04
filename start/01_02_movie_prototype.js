1e235c2477549470ca16661462d6f781c2c64a3c
PrasantRana <144428240+PrasantRana@users.noreply.github.com>
2023-11-04 23:31:41 +0000

    MovieObject

--- start/01_02_movie_prototype.js
+++ start/01_02_movie_prototype.js
@@ -15,15 +15,15 @@
 class Movie{
   constructor(title, director, genre, releaseYear, rating){
     this.title = title;
-  this.director = director;
-  this.genre = genre;
-  this.releaseYear = releaseYear;
-  this.rating = rating;
+    this.director = director;
+    this.genre = genre;
+    this.releaseYear = releaseYear;
+    this.rating = rating;
   }
 
   getOverview(){
     return `${this.title}, a ${this.genre} film directed by ${this.director} was released in
-  ${this.releaseYear}. It received a rating of ${this.rating}`
+    ${this.releaseYear}. It received a rating of ${this.rating}`
   }
 }
 
