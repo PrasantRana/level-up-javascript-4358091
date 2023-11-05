b98834d960742cafdadc4be5e23face70e950898
PrasantRana <144428240+PrasantRana@users.noreply.github.com>
2023-11-05 03:32:09 +0000

    FoodTruck

--- start/01_09_coffee_calculations.js
+++ start/01_09_coffee_calculations.js
@@ -1,2 +1,9 @@
 // Write your code here
-console.log("hello!")
+function coffeeDate(coffeArr){
+  let coffeeSum = coffeArr.reduce(
+    (totalCoffees, numCoffees) => (totalCoffees += numCoffees)
+  );
+  return `The total bill is $${coffeeSum * 1.25}.`;
+}
+
+console.log(coffeeDate([2, 5, 7, 1, 4]));
