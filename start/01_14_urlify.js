fafef663f8d85c1164db7fec2534293d63258d2a
PrasantRana <144428240+PrasantRana@users.noreply.github.com>
2023-11-05 05:09:48 +0000

    URLify(StringManipulation)

--- start/01_14_urlify.js
+++ start/01_14_urlify.js
@@ -1,1 +1,15 @@
 // Write your code here
+function urlify(blogtitle){
+  // const punctuation = /regex/;
+  // const punctuation = new RegExp("regex");
+  const punctuation = /[.,/#!$%^&*;:{}=!\-_`~()'"]/g;
+
+  const blogTitleWithoutPunctuation = blogtitle.replace(punctuation,"");
+  return blogTitleWithoutPunctuation.toLowerCase().trim().replaceAll(" ", "-");
+
+  // blogTitle.replace(charactersToReplace, charactersToReplaceRemoved);
+
+}
+
+console.log(urlify("How i got into programming!!!"))
+console.log(urlify("i have got a new job :"))
