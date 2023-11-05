ad91a0181d9ede71eb5ded739f7fb496a8be0dc7
PrasantRana <144428240+PrasantRana@users.noreply.github.com>
2023-11-05 00:16:19 +0000

    PrivateObject

--- start/01_05_private_object.js
+++ start/01_05_private_object.js
@@ -1,42 +1,12 @@
-
-class Book{
-constructor(title, author, ISBN, numCopies){
-  this.title = title;
-  this.author = author;
-  this.ISBN = ISBN;
-  this.numCopies = numCopies;
-}
-
-get availabilty (){
-  return this.getAvailability();
-}
-getAvailability = function (){
-  if (this.numCopies === 0){
-    return "Out of stock";
-  }else if (this.numCopies < 10){
-    return "Low stock"
-  } 
-  return "in stock";
-}
-restock = (numCopiesStocked = 5)=>{
-  this.numCopies += numCopiesStocked;
-}
-sell = (numCopiesSold = 1)=>{
-  this.numCopies -= numCopiesSold;
-}
-}
-
 // Write your code here
+const username = Symbol("username");
+const password = Symbol("password");
 
-class TechnicalBook extends Book{
-  constructor(title, author, ISBN, numCopies, edition){
-    super(title, author, ISBN, numCopies);
-    this.edition = edition;
-  }
+const user ={
+  [username] : "emmabostian",
+  [password] : "1234566",
+  age : 27,
+};
 
-  getEdition(){
-    return `The current version of this boo is ${this.edition}`;
-  }
-}
-
-const CrackingTheCodingInterview = new TechnicalBook("Cracking The Coding Interview", )
+console.log(user.username);
+console.log(user.password);
