33b5988c8709074f6a8e3fe8d6e8ddea4e60984e
PrasantRana <144428240+PrasantRana@users.noreply.github.com>
2023-11-05 02:32:29 +0000

    ClassroomAttendance

--- start/01_07_classroom_attendance.js
+++ start/01_07_classroom_attendance.js
@@ -1,1 +1,17 @@
 // Write your code here
+function getStudents(classroom){
+  let {hasTeachingAssistant, classList} = classroom;
+  let teacher, teachingAssistant, students;
+
+  if (hasTeachingAssistant){
+    [teacher, teachingAssistant,...students] = classList;
+  }else{
+    [teacher,...students] = classList;
+  }
+  return teacher;
+  // return teachingAssistant;
+  // return students;
+}
+
+console.log(getStudents({hasTeachingAssistant: true, classList : ["Prasant", "Priyanka","Prisha",
+"Jiwan","Misal","Maya"]}));
